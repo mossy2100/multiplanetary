0fbc0b1dd17561e52d03ab9d90beb7999fdf343c
Shaun Moss <shaun@astromultimedia.com>
2017-02-10 05:12:52 +0530

    Updated to new theme, and updated libraries module.

---
+++ themes/custom/mdc/config.rb
@@ -0,0 +1,10 @@
+http_path = "/themes/custom/mdc"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "bootstrap/assets/fonts/bootstrap"
+generated_images_dir = "images"
+http_images_path = http_path + "/" + generated_images_dir
+http_generated_images_path = http_images_path
+output_style = (environment == :production) ? :compressed : :expanded
---
+++ themes/custom/mdc/js/fix-heights.js
@@ -0,0 +1,29 @@
+/**
+ * Created by shaun on 6/2/17.
+ */
+
+(function($) {
+
+  function fixHeights() {
+    var content = $('#content');
+    var leftSidebar = $('#left-sidebar');
+
+    var contentHeight = content.height();
+    var leftSidebarHeight = leftSidebar.height();
+    var windowHeight = $(window).height();
+    var headerHeight = $('#header').height();
+    var footerHeight = $('footer').height();
+    var toolbarHeight = $('#toolbar-administration').size() ? 78 : 0;
+    var minContentHeight = windowHeight - toolbarHeight - headerHeight - footerHeight;
+
+    // Get the desired outer height of the left sidebar and the content.
+    var height = Math.max(contentHeight, leftSidebarHeight, minContentHeight);
+
+    // Set the heights.
+    leftSidebar.height(height);
+    content.height(height);
+  }
+
+  $(fixHeights);
+
+})(jQuery);
