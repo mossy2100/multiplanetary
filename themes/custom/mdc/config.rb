bc343bed888964f4b9a0d6229ccd09b208a9d215
Shaun Moss <shaun@astromultimedia.com>
2017-05-04 13:30:26 -0500

    Updated core

--- themes/custom/mdc/config.rb
+++ themes/custom/mdc/config.rb
@@ -8,3 +8,4 @@ generated_images_dir = "images"
 http_images_path = http_path + "/" + generated_images_dir
 http_generated_images_path = http_images_path
 output_style = (environment == :production) ? :compressed : :expanded
+require 'compass'
--- themes/custom/mdc/js/fix-heights.js
+++ themes/custom/mdc/js/fix-heights.js
@@ -1,5 +1,7 @@
 /**
- * Created by shaun on 6/2/17.
+ * Created by shaun on 6/2/2017.
+ *
+ * Make the sidebar and content region heights all match.
  */
 
 (function($) {
@@ -7,26 +9,38 @@
   function fixHeights() {
     var content = $('#content');
     var leftSidebar = $('#left-sidebar');
+    var rightSidebar = $('#right-sidebar');
 
-    // Get the heights.
-    var contentHeight = content.height();
-    var leftSidebarHeight = leftSidebar.height();
+    // Reset the heights.
+    content.css("height", "");
+    leftSidebar.css("height", "");
+    rightSidebar.css("height", "");
 
-    // Get the minimum content height to span the full height of the viewport.
-    var windowHeight = $(window).height();
-    var headerHeight = $('#header').height();
-    var footerHeight = $('.footer').height();
-    var toolbarHeight = $('#toolbar-administration').size() ? 78 : 0;
-    var minContentHeight = windowHeight - toolbarHeight - headerHeight - footerHeight;
+    // If not in mobile/narrow display mode, update the heights to match.
+    if (leftSidebar.css('border-radius') != '0px') {
+      // Get the heights.
+      var contentHeight = content.height() + 30;
+      var leftSidebarHeight = leftSidebar.height();
+      var rightSidebarHeight = rightSidebar.height();
 
-    // Get the desired heights.
-    var height = Math.max(contentHeight, leftSidebarHeight, minContentHeight);
+      // Get the minimum content height to span the full height of the viewport.
+      var windowHeight = $(window).height();
+      var headerHeight = $('#header').height();
+      var footerHeight = $('.footer').height();
+      var toolbarHeight = $('#toolbar-administration').size() ? 78 : 0;
+      var minContentHeight = windowHeight - toolbarHeight - headerHeight - footerHeight;
 
-    // Set the heights.
-    leftSidebar.height(height);
-    content.height(height);
+      // Get the desired heights.
+      var height = Math.max(contentHeight, leftSidebarHeight, rightSidebarHeight, minContentHeight);
+
+      // Set the heights.
+      content.height(height - 30);
+      leftSidebar.height(height);
+      rightSidebar.height(height);
+    }
   }
 
   $(window).load(fixHeights);
+  $(window).resize(fixHeights);
 
 })(jQuery);
